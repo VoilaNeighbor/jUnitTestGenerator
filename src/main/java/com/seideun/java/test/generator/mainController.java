ff51039dd4e95bd430fddd0ccf52b7ccf4c07ad6
Seideun <seideun@qq.com>
2021-12-15 22:33:54 +0800

    p. Setup Soot.

--- src/main/java/com/seideun/java/test/generator/mainController.java
+++ src/main/java/com/seideun/java/test/generator/mainController.java
@@ -16,19 +16,20 @@ public class mainController {
 
 	public static void main(String[] ch) {
 		String clspath = System.getProperty("user.dir") + File.separator +
-      "target" +
+			"target" +
 			File.separator + "test-classes";
 		String clsName = "cut.LogicStructure";
 		String methodName = "multipleIf";
 
 		UnitGraph ug = SootCFG.getMethodCFG(clspath, clsName, methodName);
-		Body body = SootCFG.getMethodBody(clspath, clsName, methodName);
+
 		//找到基础路径
 		List<List<Unit>> primerPath = SootCFGAnalyzer.findPrimePaths(ug);
+
 		//根据基础路径，扩展成完整路径(并且找到了constrains)，去掉了重复的完整路径
 		//todo: 完整路径的contrains 有问题，没有找到满足可以进行n次循环的
-		List<Path> completeTestPath = SootCFGAnalyzer.findCompleteTest(primerPath,
-			ug);
+		List<Path> completeTestPath
+			= SootCFGAnalyzer.findCompleteTest(primerPath, ug);
 
 		//输出看一下
 		int i = 1;
@@ -42,24 +43,6 @@ public class mainController {
 			System.out.println("constrains: " + p.completePathConstraint);
 			System.out.println(" ");
 		}
-		//Build the CFG and run the analysis
-//        UnitGraph g = new ExceptionalUnitGraph(body);
-
-
-//        VeryBusyExpressions an = new SimpleVeryBusyExpressions(g);
-//        Iterator i  = g.iterator();
-//        int q = 1;
-//        while(i.hasNext()){
-//            Unit u  = (Unit)i.next();
-//            List IN = an.getBusyExpressionsBefore(u);
-//            List OUT = an.getBusyExpressionsAfter(u);
-//            System.out.println("num: "+q);
-//            System.out.println("u:"+u);
-//            System.out.println("out:"+OUT);
-//            System.out.println("In:"+IN);
-//            System.out.println(" ");
-//            q +=1;
-//        }
 
 		//求解
 		List<Object> testValue = new ArrayList<>();
