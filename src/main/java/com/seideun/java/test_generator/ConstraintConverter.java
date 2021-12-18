be3bb0816bb442ccccd4ab6489c05564162d1cd2
Seideun <seideun@qq.com>
2021-12-18 08:52:55 +0800

    r.

--- src/main/java/com/seideun/java/test_generator/ConstraintConverter.java
+++ src/main/java/com/seideun/java/test_generator/ConstraintConverter.java
@@ -1,9 +1,6 @@
 package com.seideun.java.test_generator;
 
-import com.microsoft.z3.ArithSort;
-import com.microsoft.z3.BoolSort;
-import com.microsoft.z3.Context;
-import com.microsoft.z3.Expr;
+import com.microsoft.z3.*;
 import soot.IntType;
 import soot.Unit;
 import soot.Value;
@@ -11,7 +8,10 @@ import soot.jimple.IntConstant;
 import soot.jimple.ParameterRef;
 import soot.jimple.internal.*;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * This class converts constraints in the form of Jimple ASTs to Z3 expressions.
@@ -19,49 +19,42 @@ import java.util.*;
 @SuppressWarnings("unchecked")
 public class ConstraintConverter {
 	private final Context z3Context = new Context();
+	private final Map<JimpleLocal, Integer> timesLocalsAssigned = new HashMap<>();
 
-	public List<Expr<?>> collectConstraints(List<Unit> path) {
-		List<Expr<?>> result = new ArrayList<>();
-		Map<JimpleLocal, Integer> timesLocalsAssigned = new HashMap<>();
-		for (int i = 0, end = path.size() - 1; i != end; ++i) {
-			Unit thisUnit = path.get(i);
-			if (thisUnit instanceof JIfStmt) {
-				JIfStmt jIfStmt = (JIfStmt) thisUnit;
-				Expr<BoolSort> z3Expr = (Expr<BoolSort>)
-					convertJValueToZ3Expr(jIfStmt.getCondition(), timesLocalsAssigned);
-				if (conditionIsTrue(jIfStmt, i, path)) {
-					result.add(z3Expr);
-				} else {
-					result.add(z3Context.mkNot(z3Expr));
+	/**
+	 * Jimple swipes out the original names of the method, replacing with i0, i1
+	 * etc. Besides, there are locals in the Units. This method sorts out the
+	 * arguments from the input, and return their names in Jimple.
+	 */
+	public static List<String> findNamesOfParameters(Iterable<Unit> input) {
+		List<String> result = new ArrayList<>();
+		for (Unit unit: input) {
+			if (unit instanceof JIdentityStmt) {
+				JIdentityStmt jIdentityStmt = (JIdentityStmt) unit;
+				if (jIdentityStmt.getRightOp() instanceof ParameterRef) {
+					result.add(jIdentityStmt.getLeftOp().toString());
 				}
-			} else if (thisUnit instanceof JAssignStmt) {
-				JAssignStmt jAssignStmt = (JAssignStmt) thisUnit;
-				JimpleLocal local = (JimpleLocal) jAssignStmt.getLeftOp();
-				timesLocalsAssigned.compute(local, (_k, v) -> v == null ? 1 : v + 1);
 			}
 		}
 		return result;
 	}
 
-	private static boolean conditionIsTrue(
-		JIfStmt node,
-		int indexOfNode,
-		List<Unit> path
-	) {
-		return node.getTarget() == path.get(indexOfNode + 1);
+	public List<Expr<? extends Sort>> collectConstraints(List<Unit> path) {
+		List<Expr<?>> result = new ArrayList<>();
+		timesLocalsAssigned.clear();
+		// JIfStmts and JGotoStmts are guaranteed to have nodes following them.
+		for (int i = 0, end = path.size() - 1; i != end; ++i) {
+			Unit thisUnit = path.get(i);
+			if (thisUnit instanceof JIfStmt) {
+				result.add(extractConstraintOf((JIfStmt) thisUnit, i, path));
+			} else if (thisUnit instanceof JAssignStmt) {
+				incrementTimesAssigned((JAssignStmt) thisUnit);
+			}
+		}
+		return result;
 	}
 
-	public Expr<?> convertJValueToZ3Expr(Value jValue) {
-		return convertJValueToZ3Expr(jValue, Collections.emptyMap());
-	}
-
-	/**
-	 * @param timesLocalsAssigned Used for name remapping
-	 */
-	private Expr<?> convertJValueToZ3Expr(
-		Value jValue,
-		Map<JimpleLocal, Integer> timesLocalsAssigned
-	) {
+	protected Expr<? extends Sort> convertJValueToZ3Expr(Value jValue) {
 		if (jValue instanceof JimpleLocal) {
 			JimpleLocal jimpleLocal = ((JimpleLocal) jValue);
 			if (jimpleLocal.getType() == IntType.v()) {
@@ -78,12 +71,10 @@ public class ConstraintConverter {
 			JGeExpr jGeExpr = (JGeExpr) jValue;
 			return z3Context.mkGe(
 				(Expr<? extends ArithSort>) convertJValueToZ3Expr(
-					jGeExpr.getOp1(),
-					timesLocalsAssigned
+					jGeExpr.getOp1()
 				),
 				(Expr<? extends ArithSort>) convertJValueToZ3Expr(
-					jGeExpr.getOp2(),
-					timesLocalsAssigned
+					jGeExpr.getOp2()
 				)
 			);
 		} else {
@@ -91,21 +82,18 @@ public class ConstraintConverter {
 		}
 	}
 
-	/**
-	 * Jimple swipes out the original names of the method, replacing with i0, i1
-	 * etc. Besides, there are locals in the Units. This method sorts out the
-	 * arguments from the input, and return their names in Jimple.
-	 */
-	public static List<String> findNamesOfParameters(Collection<Unit> input) {
-		List<String> result = new ArrayList<>();
-		for (Unit unit: input) {
-			if (unit instanceof JIdentityStmt) {
-				JIdentityStmt jIdentityStmt = (JIdentityStmt) unit;
-				if (jIdentityStmt.getRightOp() instanceof ParameterRef) {
-					result.add(jIdentityStmt.getLeftOp().toString());
-				}
-			}
-		}
-		return result;
+	private Expr<BoolSort> extractConstraintOf(
+		JIfStmt theIf, int idxInPath, List<Unit> path
+	) {
+		Expr<BoolSort> z3Expr = (Expr<BoolSort>)
+			convertJValueToZ3Expr(theIf.getCondition());
+		return theIf.getTarget() != path.get(idxInPath + 1)
+			? z3Context.mkNot(z3Expr)
+			: z3Expr;
+	}
+
+	private void incrementTimesAssigned(JAssignStmt jAssignStmt) {
+		JimpleLocal local = (JimpleLocal) jAssignStmt.getLeftOp();
+		timesLocalsAssigned.compute(local, (k, v) -> 1 + (v == null ? 0 : v));
 	}
 }
