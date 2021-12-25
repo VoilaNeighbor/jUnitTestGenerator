0d54a2ca3cac37236795d1ee666764a75270d057
Seideun <seideun@qq.com>
2021-12-25 14:05:31 +0800

    Make arbitrary input if solver did not find answers.

--- src/main/java/com/seideun/java/test/generator/constriant_solver/NewConstraintSolver.java
+++ src/main/java/com/seideun/java/test/generator/constriant_solver/NewConstraintSolver.java
@@ -1,22 +1,50 @@
 package com.seideun.java.test.generator.constriant_solver;
 
+import com.microsoft.z3.Context;
+import com.microsoft.z3.IntExpr;
+import com.microsoft.z3.IntNum;
 import soot.Unit;
 import soot.jimple.internal.JIdentityStmt;
 import soot.jimple.internal.JimpleLocal;
 
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.List;
 
 public class NewConstraintSolver {
+	// A conflict between z3 and beautiful code is that z3 requires you to call
+	// mkXXX and remember the symbols. This forces you to introduce cache
+	// collections.
+	private Context z3Context = new Context();
+
 	/**
-	 * @param path Starting from the method entrance.
+	 * @param path Starting from the method entrance. Is a list to enforce
+	 *             orderliness.
 	 * @return Input symbols in order of encounter.
 	 */
-	public List<JimpleLocal> findAllInputSymbols(Collection<Unit> path) {
+	public List<JimpleLocal> findAllInputSymbols(List<Unit> path) {
 		// Input locals are defined by JIdentityStmt in Soot.
-		return path.stream()
+		return path.stream().sequential()
 			.filter(x -> x instanceof JIdentityStmt)
 			.map(x -> ((JimpleLocal) ((JIdentityStmt) x).getLeftOp()))
 			.toList();
+	}
+
+	public List<Object> solveSymbols(
+		List<JimpleLocal> inputSymbols,
+		List<Unit> path
+	) {
+		// Sometimes, the symbol solver is unable to soundly solve a set of
+		// constraints, nor can it prove it unsatisfiable. In this case, we
+		// assign an arbitrary value to the mysterious symbols, instead of
+		// giving up the whole case.
+		//
+		// We check if there are some unsolved symbols by examining the
+		// returned model to see if the symbol is still an Expr, not a concrete
+		// value.
+		var result = new ArrayList<Object>();
+		for (var i: inputSymbols) {
+			result.add(0);
+		}
+		return result;
 	}
 }
--- src/test/java/com/seideun/java/test/generator/constriant_solver/NewConstraintSolverTest.java
+++ src/test/java/com/seideun/java/test/generator/constriant_solver/NewConstraintSolverTest.java
@@ -2,8 +2,10 @@ package com.seideun.java.test.generator.constriant_solver;
 
 import org.junit.jupiter.api.Test;
 import soot.jimple.internal.AbstractDefinitionStmt;
+import soot.jimple.internal.JimpleLocal;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static com.seideun.java.test.generator.CFG_analyzer.SootCFGAnalyzer.findPrimePaths;
 import static com.seideun.java.test.generator.constriant_solver.SootAgent.exampleCfg;
@@ -24,5 +26,20 @@ class NewConstraintSolverTest {
 		var result = solver.findAllInputSymbols(thePath);
 
 		assertEquals(inputSymbols, result);
+	}
+
+	@Test
+	void makeArbitraryForUnboundedInput() {
+		var exampleGraph = exampleCfg("twoArgs");
+		var thePath = new ArrayList<>(findPrimePaths(exampleGraph).get(0));
+		var inputSymbols = new ArrayList<JimpleLocal>();
+		var units = exampleGraph.getBody().getUnits().stream().toList();
+		inputSymbols.add((JimpleLocal) ((AbstractDefinitionStmt) units.get(0)).getLeftOp());
+		inputSymbols.add((JimpleLocal) ((AbstractDefinitionStmt) units.get(1)).getLeftOp());
+
+		var result = solver.solveSymbols(inputSymbols, thePath);
+
+		assertEquals(Integer.class, result.get(0).getClass());
+		assertEquals(Integer.class, result.get(1).getClass());
 	}
 }
