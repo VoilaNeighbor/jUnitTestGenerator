dcfd301ff96e923ce0ba265946397d24d2f38bba
Seideun <seideun@qq.com>
2021-12-25 14:11:59 +0800

    Make arbitrary input if solver did not find answers.

--- src/test/java/com/seideun/java/test/generator/constriant_solver/NewConstraintSolverTest.java
+++ src/test/java/com/seideun/java/test/generator/constriant_solver/NewConstraintSolverTest.java
@@ -3,9 +3,11 @@ package com.seideun.java.test.generator.constriant_solver;
 import org.junit.jupiter.api.Test;
 import soot.jimple.internal.AbstractDefinitionStmt;
 import soot.jimple.internal.JimpleLocal;
+import soot.toolkits.graph.UnitGraph;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static com.seideun.java.test.generator.CFG_analyzer.SootCFGAnalyzer.findPrimePaths;
 import static com.seideun.java.test.generator.constriant_solver.SootAgent.exampleCfg;
@@ -18,14 +20,10 @@ class NewConstraintSolverTest {
 	void findAllInputSymbols() {
 		var exampleGraph = exampleCfg("twoArgs");
 		var thePath = new ArrayList<>(findPrimePaths(exampleGraph).get(0));
-		var inputSymbols = new ArrayList<>();
-		var units = exampleGraph.getBody().getUnits().stream().toList();
-		inputSymbols.add(((AbstractDefinitionStmt) units.get(0)).getLeftOp());
-		inputSymbols.add(((AbstractDefinitionStmt) units.get(1)).getLeftOp());
 
 		var result = solver.findAllInputSymbols(thePath);
 
-		assertEquals(inputSymbols, result);
+		assertEquals(fakeFindInputSymbols(exampleGraph, 2), result);
 	}
 
 	@Test
@@ -41,5 +39,21 @@ class NewConstraintSolverTest {
 
 		assertEquals(Integer.class, result.get(0).getClass());
 		assertEquals(Integer.class, result.get(1).getClass());
+	}
+
+	/**
+	 * Jimple method body always put the input symbols at the start of a method.
+	 * If we know what method we are testing against, we can extract them
+	 * directly.
+	 *
+	 * @param graph Example graph.
+	 * @param numArgs Number of arguments we know a priori.
+	 * @return List of input symbols
+	 */
+	static List<JimpleLocal> fakeFindInputSymbols(UnitGraph graph, int numArgs) {
+		var units = graph.getBody().getUnits().stream().toList();
+		return IntStream.range(0, numArgs).mapToObj(i ->
+			((JimpleLocal) ((AbstractDefinitionStmt) units.get(i)).getLeftOp())
+		).toList();
 	}
 }
