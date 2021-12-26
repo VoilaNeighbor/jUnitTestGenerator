4c1d982c166bf9b0248e9fd023c8532292c543fb
Seideun <seideun@qq.com>
2021-12-26 14:24:49 +0800

    Assigning array lengths build up a mapping (Z3 does not have array length constraints)

--- src/main/java/com/seideun/java/test/generator/new_constraint_solver/JimpleAwareZ3Context.java
+++ src/main/java/com/seideun/java/test/generator/new_constraint_solver/JimpleAwareZ3Context.java
@@ -1,9 +1,7 @@
 package com.seideun.java.test.generator.new_constraint_solver;
 
+import com.microsoft.z3.*;
 import com.microsoft.z3.Context;
-import com.microsoft.z3.Expr;
-import com.microsoft.z3.RealExpr;
-import com.microsoft.z3.Sort;
 import com.seideun.java.test.generator.constriant_solver.Rational;
 import com.seideun.java.test.generator.constriant_solver.TodoException;
 import soot.*;
@@ -20,11 +18,19 @@ import java.util.Map;
 
 /**
  * Hi! I'm a special Z3 Context which also knows Jimple well!
+ *
+ * <h2>Terminology</h2>
+ *
+ * <ul>
+ *   <li>jXx: Jimple-related concepts.</li>
+ *   <li>xxSymbol: Z3 Symbols</li>
+ *   <li>XYMap: A table from X to Y.</li>
+ * </ul>
  */
 @SuppressWarnings("unchecked")
 public class JimpleAwareZ3Context extends Context {
 	private final Map<JimpleLocal, Expr<Sort>> symbolMap = new HashMap<>();
-	private final Map<JimpleLocal, Expr<Sort>> arrNameToLenMap = new HashMap<>();
+	private final Map<JimpleLocal, Expr<Sort>> jArrToLenSymbolMap = new HashMap<>();
 
 	public Expr<Sort> getSymbol(JimpleLocal jimple) {
 		return symbolMap.get(jimple);
@@ -41,7 +47,7 @@ public class JimpleAwareZ3Context extends Context {
 	 * JimpleLocal -> Current len symbol in Z3
 	 */
 	public Map<JimpleLocal, Expr<Sort>> lengthMap() {
-		return arrNameToLenMap;
+		return jArrToLenSymbolMap;
 	}
 
 	public Expr[] add(List<Switchable> jConstraints) {
@@ -76,20 +82,21 @@ public class JimpleAwareZ3Context extends Context {
 			case DoubleConstant x -> mkReal(x.value);
 			case FloatConstant x -> mkReal(x.value);
 			case JimpleLocal x -> getOrMakeSymbol(x);
-			case JAssignStmt x -> {
-				if (x.getRightOp() instanceof JLengthExpr y) {
-					var jArray = ((JimpleLocal) y.getOp());
-					var lenSymbol = add(x.getLeftOp());
-					arrNameToLenMap.put(jArray, lenSymbol);
-					// The constraint of array lengths is not maintained in Z3.
-					yield mkTrue();
-				} else {
-					yield mkEq(add(x.getLeftOp()), add(x.getRightOp()));
-				}
-			}
+			case JAssignStmt x -> extractConstraint(x);
 			// JLengthExpr can only appear in a JAssignStmt.
 			default -> throw new TodoException(jimpleValue);
 		};
+	}
+
+	private BoolExpr extractConstraint(JAssignStmt x) {
+		if (x.getRightOp() instanceof JLengthExpr y) {
+			// The constraint of array lengths is not maintained in Z3.
+			// Instead, we keep a map from JArrays to Length Symbols.
+			jArrToLenSymbolMap.put(((JimpleLocal) y.getOp()), add(x.getLeftOp()));
+			return mkTrue();
+		} else {
+			return mkEq(add(x.getLeftOp()), add(x.getRightOp()));
+		}
 	}
 
 	private Expr<Sort> getOrMakeSymbol(JimpleLocal x) {
