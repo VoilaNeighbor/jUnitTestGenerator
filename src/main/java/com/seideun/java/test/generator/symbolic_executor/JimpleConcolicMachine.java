33d3c017f320fd8076c4b6a5190daa30a365d154
Seideun <seideun@qq.com>
2021-12-27 02:02:28 +0800

    Extract fields.

--- src/main/java/com/seideun/java/test/generator/symbolic_executor/JimpleConcolicMachine.java
+++ src/main/java/com/seideun/java/test/generator/symbolic_executor/JimpleConcolicMachine.java
@@ -27,20 +27,22 @@ import java.util.*;
 @SuppressWarnings("rawtypes")
 public class JimpleConcolicMachine {
 	// public for test now.
-	public final Context z3 = new Context();
+	private final Context z3 = new Context();
 	private final Solver solver = z3.mkSolver();
+	private UnitGraph jProgram;
 
 	/**
 	 * @return Concrete values, one table for each possible path.
 	 */
 	public List<Map<JimpleLocal, Object>> run(UnitGraph jProgram) {
+		this.jProgram = jProgram;
 		// By marking all symbols at the start, we save quite a lot of
 		// online-checking burdens.
-		var allJVars = markAllJVars(jProgram);
-		return walkGraph(jProgram, allJVars);
+		var allJVars = markAllJVars();
+		return walkGraph(allJVars);
 	}
 
-	private Map<JimpleLocal, Expr> markAllJVars(UnitGraph jProgram) {
+	private Map<JimpleLocal, Expr> markAllJVars() {
 		var result = new HashMap<JimpleLocal, Expr>();
 		for (var jVar: jProgram.getBody().getLocals()) {
 			result.put((JimpleLocal) jVar, switch (jVar.getType()) {
@@ -61,59 +63,62 @@ public class JimpleConcolicMachine {
 		}
 	}
 
-	private List<Map<JimpleLocal, Object>> walkGraph(UnitGraph jProgram, Map<JimpleLocal, Expr> allJVars) {
+	private List<Map<JimpleLocal, Object>> walkGraph(Map<JimpleLocal, Expr> allJVars) {
 		var allPaths = new ArrayList<Map<JimpleLocal, Expr>>();
 		var allConcreteValues = new ArrayList<Map<JimpleLocal, Object>>();
 		for (Unit head: jProgram.getHeads()) {
 			solver.push();
-			walkPath(head, new HashMap<>(allJVars), allPaths, allConcreteValues, jProgram, solver);
+			walkPath(head, new HashMap<>(allJVars), allPaths, allConcreteValues);
 			solver.pop();
 		}
 		return allConcreteValues;
 	}
 
-	private static void walkPath(
+	private void walkPath(
 		Unit thisUnit,
 		Map<JimpleLocal, Expr> symbolTable,
 		// Let's replace this
 		List<Map<JimpleLocal, Expr>> completedPaths,
-		List<Map<JimpleLocal, Object>> allConcreteValues,
+		List<Map<JimpleLocal, Object>> allConcreteValues
 		// By this: List<Map<JimpleLocal, Object>>
-		UnitGraph jProgram,
-		Solver solver
 	) {
 		var successors = jProgram.getSuccsOf(thisUnit);
 		if (successors.isEmpty()) {
-			var status = solver.check();
-			if (status != Status.SATISFIABLE) {
-				throw new TodoException(status);
-			}
-			var model = solver.getModel();
-
-			var parameters = jProgram.getBody().getParameterLocals();
-			var concreteValues = new HashMap<JimpleLocal, Object>();
-			for (Local i: parameters) {
-				var parameter = (JimpleLocal) i;
-				var symbolicValue = symbolTable.get(parameter);
-				var interpretation = model.eval(symbolicValue, true);
-				if (interpretation instanceof IntNum x) {
-					concreteValues.put(parameter, x.getInt());
-				} else if (interpretation instanceof SeqExpr<?> x){
-					concreteValues.put(parameter, x.getString());
-				} else {
-					throw new TodoException(interpretation);
-				}
-			}
-			allConcreteValues.add(concreteValues);
-
-			// Solve constraints and get concrete values?
+			allConcreteValues.add(solve(symbolTable));
 			completedPaths.add(symbolTable);
 		} else if (successors.size() == 1) {
-			walkPath(successors.get(0), symbolTable, completedPaths, allConcreteValues, jProgram, solver);
+			walkPath(successors.get(0), symbolTable, completedPaths, allConcreteValues);
 		} else {
 			for (Unit successor: successors) {
-				walkPath(successor, new HashMap<>(symbolTable), completedPaths, allConcreteValues, jProgram, solver);
+				walkPath(successor, new HashMap<>(symbolTable), completedPaths, allConcreteValues);
 			}
 		}
+	}
+
+	private Map<JimpleLocal, Object> solve(
+		Map<JimpleLocal, Expr> symbolTable
+	) {
+		var status = solver.check();
+		if (status != Status.SATISFIABLE) {
+			throw new TodoException(status);
+		}
+		var model = solver.getModel();
+
+		var parameters = jProgram.getBody().getParameterLocals();
+		var concreteValues = new HashMap<JimpleLocal, Object>();
+		for (Local i: parameters) {
+			var parameter = (JimpleLocal) i;
+			var symbolicValue = symbolTable.get(parameter);
+			var interpretation = model.eval(symbolicValue, true);
+			if (interpretation instanceof IntNum x) {
+				concreteValues.put(parameter, x.getInt());
+			} else if (interpretation instanceof SeqExpr<?> x){
+				concreteValues.put(parameter, x.getString());
+			} else {
+				throw new TodoException(interpretation);
+			}
+		}
+
+		return concreteValues;
 	}
 }
